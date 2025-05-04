acf36c3761f8f48f9503636d71f5c44b97f6e42a
Hasibur14 <hasib141415@gmail.com>
2025-05-04 17:08:32 +0600

    update

--- src/Pages/Home/Section/Blogs.jsx
+++ src/Pages/Home/Section/Blogs.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='bg-base-200 py-16'>
             <div>
                 <div className="text-center mb-12">
                     <h4 className='font-shadows text-primary-light'>Latest News</h4>
                     <h2 className="text-4xl font-bold text-gray-900 mb-4">Check out our blog posts</h2>
                 </div>
             </div>
+
+            <h2>Comming....</h2>
         </div>
     )
 }

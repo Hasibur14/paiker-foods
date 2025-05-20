ceaa42670ace8d3be7d675d7b3f0850ea274ed9d
Hasibur14 <hasib141415@gmail.com>
2025-05-20 15:26:59 +0600

    [ADD] here and about data get in db

--- src/Pages/Home/Section/About.jsx
+++ src/Pages/Home/Section/About.jsx
@@ -3,35 +3,30 @@ import icon1 from '../../../assets/home/leaves 1.png'
 import icon2 from '../../../assets/home/leaves 2.png'
 import icon3 from '../../../assets/home/leaves 3.png'
 import Container from '../../../components/Container/Container'
+import LoadingSpinner from '../../../components/LoadingSpinner/LoadingSpinner'
+import { useQuery } from '@tanstack/react-query'
+import useAxiosPublic from '../../../hooks/useAxiosPublic'
 
 const About = () => {
-    const aboutData = [
-        {
-            _id: 1,
-            image: icon1,
-            title: "Environmentally Friendly",
-            description: "Appropriately actualize error-free niches via efficient alignments. Professionally evolve inexpensive."
+    const axiosPublic = useAxiosPublic();
+
+
+    const { data: aboutData = [], isLoading } = useQuery({
+        queryKey: ["aboutHome"],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/aboutHome");
+            return res.data;
         },
-        {
-            _id: 2,
-            image: icon2,
-            title: "100% Organic Products",
-            description: "Appropriately actualize error-free niches via efficient alignments. Professionally evolve inexpensive."
-        },
-        {
-            _id: 3,
-            image: icon3,
-            title: "Absolute Quality",
-            description: "Appropriately actualize error-free niches via efficient alignments. Professionally evolve inexpensive."
-        }
-    ]
+    });
+
+    if (isLoading) return <LoadingSpinner />
 
     return (
         <div className="md:pt- lg:pt-16">
             <Container>
                 <div className='bg-gradient-to-t from-[#0D421D] to-[#1C6831] rounded-2xl p-4 md:p-8'>
                     <div className="flex flex-col md:flex-row justify-center items-center md:items-stretch gap-8 md:gap-4 lg:gap-10 border border-dashed rounded-2xl px-3 md:px-8 py-8 md:py-14">
-                        {aboutData.map((item, index) => (
+                        {aboutData?.map((item, index) => (
                             <div key={item._id} className="flex flex-col md:flex-row items-center w-full md:w-auto">
                                 <div className="flex flex-col items-center text-center max-w-md space-y-4 px-4 md:px-0">
                                     <img

02fefddbefaa1c4cfda7e97707233fd265bcd7c9
Hasibur14 <hasib141415@gmail.com>
2025-05-20 15:38:01 +0600

    [ADD] future development data get in db

--- src/Pages/Home/Section/FeatureDepertment.jsx
+++ src/Pages/Home/Section/FeatureDepertment.jsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react';
 import Container from '../../../components/Container/Container';
 import Button from '../../../components/Button/Button';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from '../../../components/LoadingSpinner/LoadingSpinner';
 
 const FeatureDepartment = () => {
-    const [data, setData] = useState(null);
+    const axiosPublic = useAxiosPublic();
 
-    useEffect(() => {
-        fetch('/feature.json')
-            .then((res) => res.json())
-            .then((json) => setData(json));
-    }, []);
 
-    if (!data) return (
-        <div className="text-center py-20">
-            <div className="animate-pulse flex flex-col items-center space-y-4">
-                <div className="h-6 w-1/3 bg-gray-200 rounded"></div>
-                <div className="h-12 w-3/4 bg-gray-200 rounded"></div>
-                <div className="h-4 w-full bg-gray-200 rounded"></div>
-                <div className="h-4 w-5/6 bg-gray-200 rounded"></div>
-            </div>
-        </div>
-    );
+    const { data: fetureData = [], isLoading } = useQuery({
+        queryKey: ["futureDevelopment"],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/futureDevelopment");
+            return res.data[0];
+        },
+    });
+
+    console.log(fetureData)
+
+    if (isLoading) return <LoadingSpinner />
 
     return (
         <div className="py-8 md:py-12">
@@ -29,19 +27,19 @@ const FeatureDepartment = () => {
                     {/* Left Image */}
                     <div className="mb-6 lg:mb-0 transform lg:rotate-[-10deg] hover:rotate-0 transition-all duration-300">
                         <img
-                            src={data.images.left}
+                            src={fetureData.leftImage}
                             alt="Farmer 1"
                             className="rounded-xl w-full max-w-xs sm:max-w-sm md:max-w-lg h-auto object-cover lg:h-[400px] xl:h-[500px]" />
                     </div>
 
                     {/* Center Text */}
                     <div className="text-center px-4 sm:px-6 max-w-2xl lg:max-w-3xl xl:max-w-4xl text-[#01312C]">
-                        <p className="text-second-deep font-medium text-base sm:text-lg mb-2">{data.tagline}</p>
+                        <p className="text-second-deep font-medium text-base sm:text-lg mb-2">{fetureData.tagline}</p>
                         <h2 className="font-poppins text-2xl sm:text-2xl md:text-3xl xl:text-4xl font-extrabold text-[#01312C] mb-4 whitespace-pre-line">
-                            {data.heading}
+                            {fetureData.heading}
                         </h2>
-                        <p className="font-shadows mb-4 text-sm sm:text-base">{data.description1}</p>
-                        <p className="mb-6 sm:mb-10 text-sm sm:text-base">{data.description2}</p>
+                        <p className="font-shadows mb-4 text-sm sm:text-base">{fetureData.description1}</p>
+                        <p className="mb-6 sm:mb-10 text-sm sm:text-base">{fetureData.description2}</p>
 
                         <Button
                             to='/products'
@@ -53,7 +51,7 @@ const FeatureDepartment = () => {
                     {/* Right Image */}
                     <div className="mt-6 lg:mt-0 transform lg:rotate-[10deg] hover:rotate-0 transition-all duration-300">
                         <img
-                            src={data.images.right}
+                            src={fetureData.rightImage}
                             alt="Farmer 2"
                             className="rounded-xl w-full max-w-xs sm:max-w-sm md:max-w-lg h-auto object-cover lg:h-[400px] xl:h-[500px]" />
                     </div>
